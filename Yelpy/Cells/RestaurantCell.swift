3cf59f20cbaf3d84f622fd832f25112dbfd2aca2
Pedro Muñoz <pmunoz@csumb.edu>
2020-09-15 17:51:20 -0700

    Initial Commit

---
+++ Yelpy/Cells/RestaurantCell.swift
@@ -0,0 +1,28 @@
+//
+//  RestaurantCell.swift
+//  Yelpy
+//
+//  Created by studentProfile on 9/15/20.
+//  Copyright © 2020 memo. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantCell: UITableViewCell {
+
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var restaurantImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
